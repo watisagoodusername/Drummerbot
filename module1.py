bfc526b25e527b8d01aeea9db3eb4d76a73519c6
watisagoodusername <144091194+watisagoodusername@users.noreply.github.com>
2024-11-01 13:37:47 +1100

    Update module1.py

--- module1.py
+++ module1.py
@@ -1,14 +1,20 @@
 import serial
 import time
+import serial.tools.list_ports
 
 def toarduino(arduino, message):
     for num in message:
         res = bytes(num, 'utf-8')
         arduino.write(res)
+        print(res)
 
 if __name__ == '__main__':
+    
+    ports = list(serial.tools.list_ports.comports())
+    for p in ports:
+        print(p)
 
-    ser = serial.Serial("COM6", 9600, timeout=1);
+    ser = serial.Serial("COM1", 9600, timeout=1);
     ser.reset_input_buffer()
 
     for i in range(10):
